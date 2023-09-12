0e2f6b87abf2084625137ce2371ed567b1d57b40
Noel Julien <juliennoel998@gmail.com>
2023-09-12 13:57:43 +0200

    commit before changing branch

--- src/app/shared/services/swagger-api.service.ts
+++ src/app/shared/services/swagger-api.service.ts
@@ -17,4 +17,16 @@ export class SwaggerApiService {
   getAll() : Observable<User[]> {
     return this._httpClient.get<User[]>(this.url)
   }
+
+  create(user : User) : Observable<User> {
+    return this._httpClient.post<User>(this.url, user);
+  }
+
+  update(id : number, user : User) : Observable<User> {
+    return this._httpClient.put<User>(this.url+id, user);
+  }
+
+  delete(id : number) : Observable<User> {
+    return this._httpClient.delete<User>(this.url+id);
+  }
 }
